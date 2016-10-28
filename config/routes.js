3c026bd02dde7210a6987a0409d7359becc939c0
lucek <lukasz.odziewa@gmail.com>
2016-10-28 22:26:15 +0200

    initial routes config

--- config/routes.js
+++ config/routes.js
@@ -1,12 +1,38 @@
 import React from 'react';
-import { Route, IndexRoute } from 'react-router';
-import App from '../src/App';
-import Photos from '../src/Photos';
-import Photo from '../src/Photo';
+import { Provider } from 'react-redux';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { createStore, combineReducers } from 'redux';
+import { syncHistoryWithStore, routerReducer } from 'react-router-redux';
 
-module.exports = (
-  <Route path="/" component={App}>
-    <IndexRoute component={Photos} />
-    <Route path="/photo" component={Photo} />
-  </Route>
+import photosReducer from '../src/reducers/photosReducer';
+import tagsReducer from '../src/reducers/tagsReducer';
+import App from '../src/components/App';
+import Photos from '../src/components/Photos';
+import Photo from '../src/components/Photo';
+
+const reducer = combineReducers({
+  photosReducer,
+  tagsReducer,
+  routing: routerReducer,
+});
+
+const store = createStore(
+  reducer,
 );
+
+const history = syncHistoryWithStore(browserHistory, store);
+
+const routes = (
+  <Provider store={store}>
+    <div>
+      <Router history={history}>
+        <Route path="/" component={App}>
+          <IndexRoute component={Photos} />
+          <Route path="/photo" component={Photo} />
+        </Route>
+      </Router>
+    </div>
+  </Provider>
+);
+
+export default routes;
--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { Router, browserHistory } from 'react-router';
+import ReactDOM from 'react-dom';
 import routes from './config/routes';
 
-render(
-  <Router routes={routes} history={browserHistory} />,
+ReactDOM.render(
+  routes,
   document.getElementById('app')
 );
