67d8792d7727f37884675f111b43d507403dddc5
lucek <lukasz.odziewa@gmail.com>
2016-10-28 23:02:24 +0200

    added PhotoGrid component

---
+++ src/components/PhotoGrid/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import SinglePhoto from './SinglePhoto';
+
+const PhotoGrid = (props) => {
+  const photos = props.photos.map((photo) => {
+    return (
+      <SinglePhoto photo={photo} />
+    );
+  });
+
+  return (
+    <div>{photos}</div>
+  );
+};
+
+PhotoGrid.propTypes = {
+  photos: React.PropTypes.array.isRequired,
+};
+
+export default PhotoGrid;
