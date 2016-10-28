67c5466b1f1326e8659685a0c2ce5b58ee9595bf
lucek <lukasz.odziewa@gmail.com>
2016-10-28 23:06:27 +0200

    renamed Photos route component to Home

---
+++ src/routes/Home/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link } from 'react-router'
+import PhotoGrid from '../../components/PhotoGrid';
+
+const Home = () => {
+  return (
+    <div>HOME</div>
+  );
+};
+
+export default Home;
