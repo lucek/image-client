c43db117f9be6ac973e710d8970ea7fb31e5a24f
lucek <lukasz.odziewa@gmail.com>
2016-10-28 23:54:31 +0200

    fixed rendering tags in Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
+import './Header.scss';
 
 const Header = (props) => {
   const tags = props.tags.map((tag) => {
     return (
-      <div key={tag.id}>
+      <div className="header__link" key={tag.id}>
         {tag.name}
       </div>
     );
   });
 
   return (
-    <div>{tags}</div>
+    <header>
+      <nav className="header__navigation">
+        {tags}
+      </nav>
+    </header>
   );
 };
 
