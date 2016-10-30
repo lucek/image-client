82e4fe60cb4656b347e9cb36b6a9294a23028f67
lucek <lukasz.odziewa@gmail.com>
2016-10-30 17:03:23 +0100

    moved downloading comments logic to Comments component

--- src/components/Comments/index.js
+++ src/components/Comments/index.js
@@ -1,23 +1,48 @@
 import React from 'react';
 import Comment from './Comment';
+import RestWrapper from '../../data/RestWrapper';
+import imgurConfig from '../../../config/imgur.config';
 import './Comments.scss';
 
-const Comments = (props) => {
-  const comments = props.comments.map((comment) => {
-    return (
-      <Comment comment={comment} />
+const Comments = React.createClass({
+  propTypes: {
+    photo: React.PropTypes.object.isRequired,
+  },
+
+  getInitialState() {
+    return ({
+      comments: [],
+    });
+  },
+
+  componentDidMount() {
+    const restWrapper = new RestWrapper();
+
+    restWrapper.get(
+      `${imgurConfig.endpoints.photo}${this.props.photo.id}/comments`,
+      this._setComments
     );
-  });
+  },
 
-  return (
-    <div className="comments">
-      {comments}
-    </div>
-  );
-};
+  _setComments(response) {
+    this.setState({
+      comments: JSON.parse(response.entity).data,
+    });
+  },
 
-Comments.propTypes = {
-  comments: React.PropTypes.array.isRequired,
-};
+  render() {
+    const comments = this.state.comments.map((comment) => {
+      return (
+        <Comment comment={comment} />
+      );
+    });
+
+    return (
+      <div className="comments">
+        {comments}
+      </div>
+    );
+  },
+});
 
 export default Comments;
--- src/routes/Photo/index.js
+++ src/routes/Photo/index.js
@@ -1,8 +1,5 @@
-import React from 'react'
+import React from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router'
-import imgurConfig from '../../../config/imgur.config';
-import RestWrapper from '../../data/RestWrapper';
 import SinglePhoto from '../../components/SinglePhoto';
 import Comments from '../../components/Comments';
 import './Photo.scss';
@@ -12,44 +9,21 @@ const StoreState = (state) => ({
   tags: state.tagsReducer.tags,
 });
 
-const Photo = React.createClass({
-  getInitialState() {
-    const photo = this.props.photos.find((photo) => {
-      return photo.id === this.props.params.photoId;
-    });
+const Photo = (props) => {
+  const currentPhoto = props.photos.find((photo) => {
+    return photo.id === props.params.photoId;
+  });
 
-    const comments = [];
-
-    return ({
-      photo,
-      comments,
-    });
-  },
-
-  componentDidMount() {
-    const restWrapper = new RestWrapper();
-
-    restWrapper.get(`${imgurConfig.endpoints.photo}${this.state.photo.id}/comments`, this._setComments);
-  },
-
-  _setComments(response) {
-    this.setState({
-      comments: JSON.parse(response.entity).data,
-    });
-  },
-
-  render() {
-    return (
-      <div className="photoRoute">
-        <div className="photoRoute__photo">
-          <SinglePhoto photo={this.state.photo} width="100%" height="auto" />
-        </div>
-        <div className="photoRoute__comments">
-          <Comments comments={this.state.comments} />
-        </div>
+  return (
+    <div className="photoRoute">
+      <div className="photoRoute__photo">
+        <SinglePhoto photo={currentPhoto} width="100%" height="auto" />
       </div>
-    );
-  },
-});
+      <div className="photoRoute__comments">
+        <Comments photo={currentPhoto} />
+      </div>
+    </div>
+  );
+};
 
 export default connect(StoreState)(Photo);
