e687d3465fd4b975cb39caf27391f5c31556cd44
lucek <lukasz.odziewa@gmail.com>
2016-10-30 16:02:55 +0100

    moved downloading photos from Home route component to PhotoGrid

--- src/components/PhotoGrid/index.js
+++ src/components/PhotoGrid/index.js
@@ -1,21 +1,73 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import RestWrapper from '../../data/RestWrapper';
 import SinglePhoto from '../SinglePhoto';
+import imgurConfig from '../../../config/imgur.config';
+import sampleSize from 'lodash.samplesize';
+import * as Actions from '../../actions';
+import LoadingWidget from '../../widgets/LoadingWidget';
 import './PhotoGrid.scss';
 
-const PhotoGrid = (props) => {
-  const photos = props.photos.map((photo) => {
+const StoreState = (state) => ({
+  photos: state.photosReducer.photos,
+});
+
+const PhotoGrid = React.createClass({
+  propTypes: {
+    photos: React.PropTypes.array.isRequired,
+  },
+
+  getInitialState() {
+    return ({
+      photosLoaded: false,
+    });
+  },
+
+  componentDidMount() {
+    this.restWrapper = new RestWrapper();
+    this._downloadPhotos();
+  },
+
+  _downloadPhotos() {
+    this.setState({
+      photosLoaded: false,
+    }, () => {
+      this.restWrapper.get(imgurConfig.endpoints.photos, this._setPhotos);
+    });
+  },
+
+  _setPhotos(response) {
+    const { dispatch } = this.props;
+    const apiResponse = JSON.parse(response.entity);
+    const photos = apiResponse.data.filter((photo) => {
+      return photo.type === 'image/jpeg';
+    });
+    const action = Actions.setPhotos(sampleSize(photos, 20));
+
+    this.setState({
+      photosLoaded: true,
+    }, () => {
+      dispatch(action);
+    });
+  },
+
+  render() {
+    let photos = <LoadingWidget />
+
+    if (this.state.photosLoaded) {
+      photos = this.props.photos.map((photo) => {
+        return (
+          <SinglePhoto key={photo.id} photo={photo} width="250px" />
+        );
+      });
+    }
+
     return (
-      <SinglePhoto key={photo.id} photo={photo} width="250px" />
+      <div className="photoGrid">
+        {photos}
+      </div>
     );
-  });
+  },
+});
 
-  return (
-    <div className="photoGrid">{photos}</div>
-  );
-};
-
-PhotoGrid.propTypes = {
-  photos: React.PropTypes.array.isRequired,
-};
-
-export default PhotoGrid;
+export default connect(StoreState)(PhotoGrid);
--- src/routes/Home/index.js
+++ src/routes/Home/index.js
@@ -1,37 +1,7 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import RestWrapper from '../../data/RestWrapper';
-import imgurConfig from '../../../config/imgur.config';
-import sampleSize from 'lodash.samplesize';
 import PhotoGrid from '../../components/PhotoGrid';
-import * as Actions from '../../actions';
-
-const StoreState = (state) => ({
-  photos: state.photosReducer.photos,
-  tags: state.tagsReducer.tags,
-});
 
 const Home = React.createClass({
-  componentDidMount() {
-    this.restWrapper = new RestWrapper();
-    this._downloadPhotos();
-  },
-
-  _downloadPhotos() {
-    this.restWrapper.get(imgurConfig.endpoints.photos, this._setPhotos);
-  },
-
-  _setPhotos(response) {
-    const { dispatch } = this.props;
-    const apiResponse = JSON.parse(response.entity);
-    const photos = apiResponse.data.filter((photo) => {
-      return photo.type === 'image/jpeg';
-    });
-    const action = Actions.setPhotos(sampleSize(photos, 20));
-
-    dispatch(action);
-  },
-
   render() {
     return (
       <PhotoGrid photos={this.props.photos} />
@@ -39,4 +9,4 @@ const Home = React.createClass({
   },
 });
 
-export default connect(StoreState)(Home);
+export default Home;
