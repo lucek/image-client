944b52fba88fead1f292766d3b8d8d181ea61914
lucek <lukasz.odziewa@gmail.com>
2016-10-31 15:11:40 +0100

    displaying tag name in tag route component

--- src/routes/Tag/index.js
+++ src/routes/Tag/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import camelCase from 'lodash.camelcase';
 import PhotoGrid from '../../components/PhotoGrid';
 import imgurConfig from '../../../config/imgur.config';
+import './Tag.scss';
 
 const StoreState = (state) => ({
   photos: state.photosReducer.photos,
@@ -25,9 +27,15 @@ const Tag = React.createClass({
 
   render() {
     const url = `${imgurConfig.endpoints.tagPhotos}${this.state.currentTagId}/viral/1`;
+    const currentTag = this.props.tags.find((tag) => {
+      return tag.id === parseInt(this.state.currentTagId);
+    });
 
     return (
-      <div>
+      <div className="tagRoute">
+        <div className="tagRoute__tagName">
+          {`#${camelCase(currentTag.name)}`}
+        </div>
         <PhotoGrid url={url} />
       </div>
     );
