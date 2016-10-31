188765cd5c1c23a21235436bb8162cf24717f15c
lucek <lukasz.odziewa@gmail.com>
2016-10-31 22:14:37 +0100

    added propTypes to photo grid component

--- src/components/PhotoGrid/index.js
+++ src/components/PhotoGrid/index.js
@@ -14,6 +14,7 @@ const StoreState = (state) => ({
 
 const PhotoGrid = React.createClass({
   propTypes: {
+    photos: React.PropTypes.photos.isRequired,
     url: React.PropTypes.string.isRequired,
   },
 
