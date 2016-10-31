0bdbbc47b95fb2358cfd8e38a2eaa6c1b6418b3a
lucek <lukasz.odziewa@gmail.com>
2016-10-31 21:55:58 +0100

    displaying photo information when available

--- src/routes/Photo/index.js
+++ src/routes/Photo/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import moment from 'moment';
 import SinglePhoto from '../../components/SinglePhoto';
 import Comments from '../../components/Comments';
 import './Photo.scss';
@@ -14,10 +15,36 @@ const Photo = (props) => {
     return photo.id === props.params.photoId;
   });
 
+  let photoTitle;
+  let photoDescription;
+
+  if (currentPhoto.title) {
+    photoTitle = (
+      <div className="photoRoute__photo__title">
+        {currentPhoto.title}
+      </div>
+    );
+  }
+
+  if (currentPhoto.description) {
+    photoDescription = (
+      <div className="photoRoute__photo__description">
+        {currentPhoto.description}
+      </div>
+    );
+  }
+
   return (
     <div className="photoRoute">
       <div className="photoRoute__photo">
+        <div className="photoRoute__photo__information">
+          {photoTitle}
+          <div className="photoRoute__photo__date">
+            {moment.unix(currentPhoto.datetime).format('DD/MM/YYYY - hh:mm')}
+          </div>
+        </div>
         <SinglePhoto photo={currentPhoto} width="100%" height="auto" />
+        {photoDescription}
       </div>
       <div className="photoRoute__comments">
         <Comments photo={currentPhoto} />
